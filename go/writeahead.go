2f736edfcc49915a3b34d358f4e95658aff1fd88
Alan Gutierrez <alan@prettyrobots.com>
2021-06-27 14:20:34 -0500

    Sketch.

--- go/writeahead.go
+++ go/writeahead.go
@@ -1,15 +1,140 @@
 package writeahead
 
-type Block struct {
+import (
+    transcript "github.com/bigeasy/transcript/go"
+    "sync"
+//    "github.com/google/vectorio"
+    "encoding/json"
+)
+
+type Write struct {
+    keys []string
+    buffer []byte
+}
+
+type _Write struct {
+    keys []string
+    buffer []byte
+    sync bool
+}
+
+type _Block struct {
+    position uint64
+    keys []string
+    buffer []byte
+}
+
+type _BlockWait struct {
+    next *_BlockWait
+    position chan uint64
+    block *_Block
+}
+
+type _BlockWrite struct {
+    keys []string
+    buffer []byte
+    position chan uint64
+}
+
+type _Log struct {
+    id int
+    shifted bool
+    lock *sync.RWMutex
+    blocks map[string][]_Block
+}
+
+type _Appender struct {
+    position uint64
+    recorder *transcript.Recorder
 }
 
 type WriteAhead struct {
     directory string
+    checksum transcript.ChecksumFunc
+    writes int
+    logs []_Log
+    writer chan _BlockWrite
+    head *_BlockWait
+    last *_BlockWait
+    appender *_Appender
 }
 
-func NewWriteahead (directory string) *WriteAhead {
-    return &WriteAhead{directory: directory}
+// https://stackoverflow.com/questions/3398490/checking-if-a-channel-has-a-ready-to-read-value-using-go
+// https://stackoverflow.com/questions/33328000/reading-multiple-elements-from-a-channel-in-go
+func _WriteAhead (appender *_Appender, writes <-chan _BlockWrite) {
+    blocks := make([]_BlockWrite, 0, 32)
+    for first := range writes {
+        blocks = append(blocks, first)
+        GATHER: for len(blocks) != cap(blocks) {
+            select {
+            case next := <-writes:
+                blocks = append(blocks, next)
+            default:
+                break GATHER
+            }
+        }
+        buffers := make([][]byte, len(blocks))
+        positions := make([]uint64, len(blocks))
+        for i, block := range blocks {
+            keys, _ := json.Marshal(block.keys)
+            buffers[i] = appender.recorder.Record([][][]byte{ [][]byte{ keys, block.buffer } })
+            positions[i] = appender.position
+    //        block := _Block{write.keys, buffers[i], writeahead.position}
+        }
+    }
 }
 
-func (writeahead *WriteAhead) Write () {
+func _ClearWrites (writeahead *WriteAhead) {
+    for writeahead.writes != 0 {
+    }
+}
+
+func NewWriteahead (directory string, checksum transcript.ChecksumFunc) *WriteAhead {
+    log := _Log{0, false, new(sync.RWMutex), make(map[string][]_Block)}
+    appender := &_Appender{ 0, transcript.NewRecorder(checksum) }
+    wait := &_BlockWait{}
+    return &WriteAhead{
+        directory: directory,
+        checksum: checksum,
+        logs: []_Log{ log },
+        writer: make(chan _BlockWrite),
+        head: wait,
+        last: wait,
+        appender: appender,
+    }
+}
+
+func (writeahead *WriteAhead) Write (writes []Write) {
+    log := writeahead.logs[len(writeahead.logs) - 1]
+    for _, write := range writes {
+        block := _Block{0, write.keys, write.buffer}
+        for _, key := range write.keys {
+            if log.blocks[key] == nil {
+                log.blocks[key] = make([]_Block, 0, 32)
+            }
+            log.blocks[key] = append(log.blocks[key], block)
+        }
+        position := make(chan uint64, 1)
+        blockWait := _BlockWait{position: position, block: &block}
+        writeahead.last.next = &blockWait
+        writeahead.last = &blockWait
+        writeahead.writes++
+        writeahead.writer <- _BlockWrite{write.keys, write.buffer, position}
+    }
+}
+
+func (writeahead *WriteAhead) Get () {
+}
+
+func (writeahead *WriteAhead) Rotate () {
+    log := _Log{0, false, new(sync.RWMutex), make(map[string][]_Block)}
+    writeahead.logs = append(writeahead.logs, log)
+}
+
+func (writeahead *WriteAhead) Shift () {
+    if len(writeahead.logs) != 0 {
+        log := writeahead.logs[0]
+        writeahead.logs = writeahead.logs[1:]
+        log.shifted = true
+    }
 }
